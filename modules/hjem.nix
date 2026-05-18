5bd008c59c4800b960a7773cb0b1d8293db98f97
System administrator <56847436+maximus44888@users.noreply.github.com>
2026-05-18 21:28:59 +0200

    Hjem update

--- modules/hjem.nix
+++ modules/hjem.nix
@@ -3,6 +3,6 @@
 # Courtesy of choco98 - https://discord.com/channels/961691461554950145/1233827370855694426/1502331749722489123
 let hjem-src = builtins.fetchTarball {
 	url = "https://github.com/feel-co/hjem/archive/main.tar.gz";
-	sha256 = "sha256:1jmrszlchrg08jwdpxmvccj8qhk8w7pnfb1yx3f0h046ys8d38c1";
+	sha256 = "sha256:1kl613afzp9mi6gpf4bz2r50slg1nd0vhj4wdm9wvbhyjnz7ax8c";
 };
 in (import "${hjem-src}/modules/nixos/default.nix").hjem
