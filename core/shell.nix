843063c6d85b16d497507fdec361dd3da0646b41
Mausimux <56847436+Mausimux@users.noreply.github.com>
2026-06-23 21:06:21 +0200

    Tmux

--- core/shell.nix
+++ core/shell.nix
@@ -1,4 +1,10 @@
-{ ... }: {
+{ pkgs, ... }: {
+
+environment.systemPackages = with pkgs; [
+	kitty
+];
+
+programs.tmux.enable = true;
 
 environment.shellAliases = {
 	nr = "sudo nixos-rebuild switch";
