111878060b137f19f0f057f279137041a24be514
Mausimux <56847436+Mausimux@users.noreply.github.com>
2026-08-08 19:45:10 +0200

    Better ls!

--- core/shell.nix
+++ core/shell.nix
@@ -1,13 +1,22 @@
-{ pkgs, ... }: {
+{ pkgs, ... }: let
+	lsArgs = "--long --all --git --icons --no-permissions --no-user --no-time --no-filesize --ignore-glob=\".git|.direnv\"";
+in {
 
 environment.systemPackages = with pkgs; [
 	kitty
 	zellij
+	eza
 ];
 
 environment.shellAliases = {
 	nr = "sudo nixos-rebuild switch";
 	ns = "nix shell";
+
+	ls = "eza ${lsArgs}";
+	l = "eza ${lsArgs}";
+	ll = "eza ${lsArgs}";
+	la = "eza ${lsArgs}";
+	lt = "eza --tree ${lsArgs}";
 };
 
 }
