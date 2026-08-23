19585e0964c8c96d45f0f18c8864d0b70a2df9d1
Mausimux <56847436+Mausimux@users.noreply.github.com>
2026-08-23 21:26:46 +0200

    NH now handles cleanup and optimization

--- core/nh.nix
+++ core/nh.nix
@@ -1,5 +1,11 @@
 { ... }: {
 
-programs.nh.enable = true;
+programs.nh = {
+	enable = true;
+	clean = {
+		enable = true;
+		extraArgs = "--keep 5 --keep-since 7d --optimise";
+	};
+};
 
 }
--- core/nix.nix
+++ core/nix.nix
@@ -7,21 +7,4 @@ nix.settings.experimental-features = [
 	"flakes"
 ];
 
-nix.gc = {
-	automatic = true;
-	dates = [ "weekly" ];
-	options = "--delete-older-than 14d";
-};
-
-nix.optimise = {
-	automatic = true;
-	dates = [ "weekly" ];
-};
-
-system.autoUpgrade = {
-	enable = true;
-	allowReboot = true;
-	dates = "daily";
-};
-
 }
