11272fe166536c5cba53597e6b99d01b362ed6f3
Mausimux <56847436+Mausimux@users.noreply.github.com>
2026-08-30 06:14:38 +0200

    Removed some comments

--- flake.nix
+++ flake.nix
@@ -1,9 +1,5 @@
 {
 	inputs = {
-		# This is pointing to an unstable release.
-		# If you prefer a stable release instead, you can change the word unstable to the latest number shown here: https://nixos.org/download
-		# i.e. nixos-24.11
-		# Use `nix flake update` to update the flake to the latest revision of the chosen release channel.
 		nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 		hjem = {
 			url = "github:feel-co/hjem";
@@ -31,7 +27,6 @@
 
 		in { inherit imports; };
 	in {
-		# NOTE: 'nixos' is the default hostname
 		nixosConfigurations.${hostname} = nixpkgs.lib.nixosSystem {
 			specialArgs = {
 				inherit inputs;
